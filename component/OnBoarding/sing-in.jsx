77cc383e1969f2dd0f39035a145a864795a3e9ec
silenteyesoncode <46851135+silenteyesoncode@users.noreply.github.com>
2020-08-14 05:42:37 +0530

    UI

--- component/OnBoarding/sing-in.jsx
+++ component/OnBoarding/sing-in.jsx
@@ -1,12 +1,28 @@
 import React from 'react';
-import { View, StyleSheet, Image, Text, TouchableOpacity } from 'react-native';
+import { View, StyleSheet, Image, Text, TouchableOpacity, TextInput } from 'react-native';
 
 const SignIn = props => {
   return(
       <View style={styles.Signin}>
-        <Text>
+        <Text style={{paddingBottom: '20%'}}>
           Let’s get you started!
         </Text>
+        <View style={{padding:20}}>
+          <Text style={styles.lable}>
+            First Name
+            </Text>
+              <TextInput
+              style={{padding: 5}}
+              placeholder="Type here to translate!" 
+            />      
+          <Text style={styles.lable}>
+            Last Name
+          </Text>
+              <TextInput
+              
+              placeholder="Type here to translate!" 
+              />
+        </View>
       </View>
   )
 };
@@ -14,8 +30,13 @@ const SignIn = props => {
 const styles = StyleSheet.create({
   Signin: {
     flex: 1,
-    justifyContent: 'space-evenly',
-
+    justifyContent: 'center',
+    flexDirection: 'column',
+    flexWrap: 'wrap'
+  },
+  lable: {
+    position:'relative',
+    padding: '5%',
   }
 });
 
